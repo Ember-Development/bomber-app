e6dcdeae4a5a58e4e653e6fc37f7b89c6d5f2ca7
braedonwatkins <watkins.braedon@gmail.com>
2025-02-19 12:43:06 -0500

    this seems to be working better... with prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,33 @@
-import config from "eslint-config-standard";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  ...[].concat(config),
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    ignores: [
+      'coverage',
+      '**/public',
+      '**/dist',
+      'pnpm-lock.yaml',
+      'pnpm-workspace.yaml',
+    ],
+  },
+
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off', // seems to be an outdated rule... smh... read more [here](https://www.reddit.com/r/react/comments/11d8xjk/srcappjs_line_2_react_must_be_in_scope_when_using/)
+      'react/no-unescaped-entities': 'warn',
+    },
+  },
+
+  //WARNING: do not move this! it has to be last for prettier to work!
+  eslintConfigPrettier,
 ];
