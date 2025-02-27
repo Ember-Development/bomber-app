a0260b36d26e7928fa354d0511499e05e24575c8
braedonwatkins <watkins.braedon@gmail.com>
2025-02-27 16:01:47 -0500

    trophy type

---
+++ packages/database/types/trophy.ts
@@ -0,0 +1,13 @@
+import { Prisma } from '../generated/client';
+
+type Relations = {
+  team: { team: true };
+};
+
+export type TrophyDynamic<R extends (keyof Relations)[]> =
+  Prisma.TrophyGetPayload<{
+    include: { [K in R[number]]: true };
+  }>;
+
+export type Trophy = TrophyDynamic<['team']>;
+export type TrophyDB = TrophyDynamic<[]>;
