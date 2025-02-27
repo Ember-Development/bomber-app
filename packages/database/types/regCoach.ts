9c677227e14f94d38a74bc31952fec99baec6944
braedonwatkins <watkins.braedon@gmail.com>
2025-02-27 15:38:24 -0500

    coach related types

---
+++ packages/database/types/regCoach.ts
@@ -0,0 +1,14 @@
+import { Prisma } from '../generated/client';
+
+type Relations = {
+  user: { user: true };
+  teams: { teams: true };
+};
+
+export type RegCoachDynamic<R extends (keyof Relations)[]> =
+  Prisma.RegCoachGetPayload<{
+    include: { [K in R[number]]: true };
+  }>;
+
+export type Admin = RegCoachDynamic<['user', 'teams']>;
+export type AdminDB = RegCoachDynamic<[]>;
