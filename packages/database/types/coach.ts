937f836870bf84281260ec6729ded94c28e41e03
braedonwatkins <watkins.braedon@gmail.com>
2025-02-27 15:38:24 -0500

    coach related types

---
+++ packages/database/types/coach.ts
@@ -0,0 +1,15 @@
+import { Prisma } from '../generated/client';
+
+type Relations = {
+  user: { user: true };
+  headTeams: { headTeams: true };
+  teams: { teams: true };
+};
+
+export type CoachDynamic<R extends (keyof Relations)[]> =
+  Prisma.CoachGetPayload<{
+    include: { [K in R[number]]: true };
+  }>;
+
+export type Admin = CoachDynamic<['user', 'headTeams', 'teams']>;
+export type AdminDB = CoachDynamic<[]>;
