fcb63a58ec0c253ebc13e37740b066cda987a4c8
braedonwatkins <watkins.braedon@gmail.com>
2025-02-27 15:58:52 -0500

    parent and player user types

---
+++ packages/database/types/parent.ts
@@ -0,0 +1,15 @@
+import { Prisma } from '../generated/client';
+
+type Relations = {
+  user: { user: true };
+  address: { address: true };
+  children: { children: true };
+};
+
+export type ParentDynamic<R extends (keyof Relations)[]> =
+  Prisma.ParentGetPayload<{
+    include: { [K in R[number]]: true };
+  }>;
+
+export type Admin = ParentDynamic<['user', 'address', 'children']>;
+export type AdminDB = ParentDynamic<[]>;
