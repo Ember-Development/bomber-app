255fafd73143c078fd671fd3c7ea39940e8fe097
braedonwatkins <watkins.braedon@gmail.com>
2025-03-03 17:59:00 -0500

    remove extra functions

--- packages/database/types/fake-data.ts
+++ packages/database/types/fake-data.ts
@@ -11,24 +11,7 @@ import {
   ShortsSize,
 } from '/Users/braedon/ember/bomber-app/packages/database/generated/client';
 import { faker } from '@faker-js/faker';
-import Decimal from 'decimal.js';
 
-export function fakeUser() {
-  return {
-    email: faker.internet.email(),
-    phone: undefined,
-    pass: faker.lorem.words(5),
-    fname: faker.lorem.words(5),
-    lname: faker.lorem.words(5),
-    primaryRole: faker.helpers.arrayElement([
-      UserRole.ADMIN,
-      UserRole.COACH,
-      UserRole.REGIONAL_COACH,
-      UserRole.PLAYER,
-      UserRole.FAN,
-    ] as const),
-  };
-}
 export function fakeUserComplete() {
   return {
     id: faker.string.uuid(),
@@ -44,92 +27,6 @@ export function fakeUserComplete() {
       UserRole.PLAYER,
       UserRole.FAN,
     ] as const),
-  };
-}
-export function fakePlayer() {
-  return {
-    pos1: faker.helpers.arrayElement([
-      Position.PITCHER,
-      Position.CATCHER,
-      Position.FIRST_BASE,
-      Position.SECOND_BASE,
-      Position.THIRD_BASE,
-      Position.SHORTSTOP,
-      Position.LEFT_FIELD,
-      Position.CENTER_FIELD,
-      Position.RIGHT_FIELD,
-      Position.DESIGNATED_HITTER,
-    ] as const),
-    pos2: faker.helpers.arrayElement([
-      Position.PITCHER,
-      Position.CATCHER,
-      Position.FIRST_BASE,
-      Position.SECOND_BASE,
-      Position.THIRD_BASE,
-      Position.SHORTSTOP,
-      Position.LEFT_FIELD,
-      Position.CENTER_FIELD,
-      Position.RIGHT_FIELD,
-      Position.DESIGNATED_HITTER,
-    ] as const),
-    jerseyNum: faker.lorem.words(5),
-    gradYear: faker.lorem.words(5),
-    jerseySize: faker.helpers.arrayElement([
-      JerseySize.YXS,
-      JerseySize.YS,
-      JerseySize.YM,
-      JerseySize.YL,
-      JerseySize.YXL,
-      JerseySize.AS,
-      JerseySize.AM,
-      JerseySize.AL,
-      JerseySize.AXL,
-      JerseySize.A2XL,
-    ] as const),
-    pantSize: faker.helpers.arrayElement([
-      PantsSize.SIZE_20,
-      PantsSize.SIZE_22,
-      PantsSize.SIZE_24,
-      PantsSize.SIZE_26,
-      PantsSize.SIZE_30,
-      PantsSize.SIZE_32,
-      PantsSize.SIZE_33,
-      PantsSize.SIZE_34,
-      PantsSize.SIZE_36,
-      PantsSize.SIZE_38,
-    ] as const),
-    stirrupSize: faker.helpers.arrayElement([
-      StirrupSize.SM,
-      StirrupSize.LG,
-      StirrupSize.XL,
-    ] as const),
-    shortSize: faker.helpers.arrayElement([
-      ShortsSize.YXL,
-      ShortsSize.ASM,
-      ShortsSize.AMD,
-      ShortsSize.ALG,
-      ShortsSize.AXL,
-      ShortsSize.A2XL,
-    ] as const),
-    practiceShortSize: faker.helpers.arrayElement([
-      ShortsSize.YXL,
-      ShortsSize.ASM,
-      ShortsSize.AMD,
-      ShortsSize.ALG,
-      ShortsSize.AXL,
-      ShortsSize.A2XL,
-    ] as const),
-    ageGroup: faker.helpers.arrayElement([
-      AgeGroup.U8,
-      AgeGroup.U10,
-      AgeGroup.U12,
-      AgeGroup.U14,
-      AgeGroup.U16,
-      AgeGroup.U18,
-      AgeGroup.ALUMNI,
-    ] as const),
-    isTrusted: undefined,
-    college: undefined,
   };
 }
 export function fakePlayerComplete() {
@@ -240,44 +137,10 @@ export function fakeCoachComplete() {
     userID: faker.string.uuid(),
   };
 }
-export function fakeRegCoach() {
-  return {
-    region: faker.helpers.arrayElement([
-      Regions.NW,
-      Regions.SW,
-      Regions.S,
-      Regions.SE,
-      Regions.NE,
-      Regions.MW,
-    ] as const),
-  };
-}
 export function fakeRegCoachComplete() {
   return {
     id: faker.string.uuid(),
     userID: faker.string.uuid(),
-    region: faker.helpers.arrayElement([
-      Regions.NW,
-      Regions.SW,
-      Regions.S,
-      Regions.SE,
-      Regions.NE,
-      Regions.MW,
-    ] as const),
-  };
-}
-export function fakeTeam() {
-  return {
-    name: faker.person.fullName(),
-    ageGroup: faker.helpers.arrayElement([
-      AgeGroup.U8,
-      AgeGroup.U10,
-      AgeGroup.U12,
-      AgeGroup.U14,
-      AgeGroup.U16,
-      AgeGroup.U18,
-      AgeGroup.ALUMNI,
-    ] as const),
     region: faker.helpers.arrayElement([
       Regions.NW,
       Regions.SW,
@@ -312,12 +175,6 @@ export function fakeTeamComplete() {
     headCoachID: faker.string.uuid(),
   };
 }
-export function fakeTrophy() {
-  return {
-    title: faker.lorem.words(5),
-    imageURL: faker.lorem.words(5),
-  };
-}
 export function fakeTrophyComplete() {
   return {
     id: faker.string.uuid(),
@@ -333,15 +190,6 @@ export function fakeParentComplete() {
     userID: faker.string.uuid(),
   };
 }
-export function fakeAddress() {
-  return {
-    state: faker.lorem.words(5),
-    city: faker.lorem.words(5),
-    zip: faker.lorem.words(5),
-    address1: faker.lorem.words(5),
-    address2: undefined,
-  };
-}
 export function fakeAddressComplete() {
   return {
     id: faker.string.uuid(),
@@ -352,20 +200,10 @@ export function fakeAddressComplete() {
     address2: undefined,
   };
 }
-export function fakeChat() {
-  return {
-    title: faker.lorem.words(5),
-  };
-}
 export function fakeChatComplete() {
   return {
     id: faker.string.uuid(),
     title: faker.lorem.words(5),
-  };
-}
-export function fakeUserChat() {
-  return {
-    joinedAt: faker.date.anytime(),
   };
 }
 export function fakeUserChatComplete() {
@@ -375,12 +213,6 @@ export function fakeUserChatComplete() {
     joinedAt: faker.date.anytime(),
   };
 }
-export function fakeMessage() {
-  return {
-    text: faker.lorem.words(5),
-    createdAt: faker.date.anytime(),
-  };
-}
 export function fakeMessageComplete() {
   return {
     id: faker.string.uuid(),
@@ -388,13 +220,6 @@ export function fakeMessageComplete() {
     createdAt: faker.date.anytime(),
     userID: faker.string.uuid(),
     chatID: faker.string.uuid(),
-  };
-}
-export function fakeNotification() {
-  return {
-    title: faker.lorem.words(5),
-    body: faker.lorem.words(5),
-    createdAt: faker.date.anytime(),
   };
 }
 export function fakeNotificationComplete() {
@@ -410,17 +235,6 @@ export function fakeUserNotificationComplete() {
     userID: faker.string.uuid(),
     notificationID: faker.string.uuid(),
     isRead: false,
-  };
-}
-export function fakeEvent() {
-  return {
-    eventType: faker.helpers.arrayElement([
-      EventType.TOURNAMENT,
-      EventType.PRACTICE,
-      EventType.GLOBAL,
-    ] as const),
-    start: faker.date.anytime(),
-    end: faker.date.anytime(),
   };
 }
 export function fakeEventComplete() {
@@ -442,13 +256,6 @@ export function fakeEventAttendanceComplete() {
     userID: faker.string.uuid(),
     eventID: faker.string.uuid(),
     status: AttendanceStatus.PENDING,
-  };
-}
-export function fakeTournament() {
-  return {
-    title: faker.lorem.words(5),
-    body: faker.lorem.words(5),
-    imageURL: faker.lorem.words(5),
   };
 }
 export function fakeTournamentComplete() {
