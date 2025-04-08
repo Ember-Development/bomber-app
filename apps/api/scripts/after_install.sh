a585735e0e548880625f42d4cfbf427415d9f103
braedonwatkins <watkins.braedon@gmail.com>
2025-04-08 13:51:33 -0400

    fix api idempotency script

--- apps/api/scripts/after_install.sh
+++ apps/api/scripts/after_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/api
-pnpm install
 
+pnpm install
+tsc
--- apps/api/scripts/start_application.sh
+++ apps/api/scripts/start_application.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-cd /var/www/api/
-pm2 restart api
+
+cd /var/www/api/dist/
+
+if pm2 list | grep -q "api"; then
+  echo "App is already running. Restarting..."
+  pm2 restart "api"
+else
+  echo "App is not running. Starting app..."
+  pm2 start "index.js" --name "api"
+fi
+
