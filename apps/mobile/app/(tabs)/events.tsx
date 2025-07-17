8d21fe13c60024b69003ece3665904632f73469a
braedonwatkins <watkins.braedon@gmail.com>
2025-07-17 17:48:05 -0400

    install and apply react-big-calendar... so far so amazing

--- apps/mobile/app/(tabs)/events.tsx
+++ apps/mobile/app/(tabs)/events.tsx
@@ -3,6 +3,20 @@ import { createGroupStyles } from '@/styles/groupsStyle';
 import { Text } from '@react-navigation/elements';
 import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Calendar } from 'react-native-big-calendar';
+
+const events = [
+  {
+    title: 'Meeting',
+    start: new Date(2025, 7, 17, 10, 0),
+    end: new Date(2025, 1, 17, 10, 30),
+  },
+  {
+    title: 'Coffee break',
+    start: new Date(2025, 7, 17, 15, 45),
+    end: new Date(2025, 7, 17, 16, 30),
+  },
+];
 
 export default function EventsScreen() {
   const styles = createGroupStyles('light');
@@ -12,6 +26,7 @@ export default function EventsScreen() {
       <SafeAreaView style={styles.safeContainer}>
         <View>
           <Text>Hello Whirl</Text>
+          <Calendar events={events} height={600} />
         </View>
       </SafeAreaView>
     </BackgroundWrapper>
