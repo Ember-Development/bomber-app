01a053f22d142307fe46fb03a1b2930340a09a65
braedonwatkins <watkins.braedon@gmail.com>
2025-07-18 15:54:18 -0400

    loading events correctly + offset minutes
    - found out event months are zero indexed (im a zero indexer defender
      but cmon now)
    - auto scroll offset minutes so whatever the current time is, that is
      where you see in the screen

--- apps/mobile/app/(tabs)/events.tsx
+++ apps/mobile/app/(tabs)/events.tsx
@@ -1,32 +1,68 @@
 import BackgroundWrapper from '@/components/ui/organisms/backgroundWrapper';
 import { createGroupStyles } from '@/styles/groupsStyle';
-import { Text } from '@react-navigation/elements';
 import { View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Calendar } from 'react-native-big-calendar';
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+import { Calendar, ICalendarEventBase } from 'react-native-big-calendar';
+import { Dimensions } from 'react-native';
 
-const events = [
-  {
-    title: 'Meeting',
-    start: new Date(2025, 7, 17, 10, 0),
-    end: new Date(2025, 1, 17, 10, 30),
-  },
-  {
-    title: 'Coffee break',
-    start: new Date(2025, 7, 17, 15, 45),
-    end: new Date(2025, 7, 17, 16, 30),
-  },
-];
+// export interface ICalendarEventBase {
+//     start: Date;
+//     end: Date;
+//     title: string;
+//     children?: ReactElement | null;
+//     hideHours?: boolean;
+//     disabled?: boolean;
+//     /**
+//      * overlapping position of event starting from 0 (optional)
+//      */
+//     overlapPosition?: number;
+//     /**
+//      * number of events overlapping with this event (optional)
+//      */
+//     overlapCount?: number;
+// }
 
 export default function EventsScreen() {
+  // TODO: figure out why the calc for the height isn't quite right
+  const insets = useSafeAreaInsets();
   const styles = createGroupStyles('light');
+  const screenHeight = Dimensions.get('window').height;
+  const headerHeight = 0;
+  const calendarHeight =
+    screenHeight - insets.top - insets.bottom - headerHeight;
+
+  const events: ICalendarEventBase[] = [
+    {
+      title: 'Meeting',
+      start: new Date(2025, 6, 18, 10, 0),
+      end: new Date(2025, 6, 18, 10, 30),
+    },
+    {
+      title: 'Coffee break',
+      start: new Date(2025, 6, 18, 15, 45),
+      end: new Date(2025, 6, 18, 16, 30),
+    },
+  ];
+
+  const curDate = new Date();
+  const curMinutes = curDate.getMinutes();
+  const curHours = curDate.getHours();
+  const offsetMinutes = curMinutes + 60 * curHours;
 
   return (
     <BackgroundWrapper>
-      <SafeAreaView style={styles.safeContainer}>
-        <View>
-          <Text>Hello Whirl</Text>
-          <Calendar events={events} height={600} />
+      <SafeAreaView
+        style={[styles.safeContainer, { paddingBottom: insets.bottom }]}
+      >
+        <View style={{ flex: 1 }}>
+          <Calendar
+            events={events}
+            height={calendarHeight}
+            scrollOffsetMinutes={offsetMinutes}
+          />
         </View>
       </SafeAreaView>
     </BackgroundWrapper>
