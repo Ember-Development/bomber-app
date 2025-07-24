64fa4f9487588b981b5eaf132639140286095154
Gunnar Smith <gunnarsmith3@gmail.com>
2025-07-23 23:14:25 -0500

    updated coach schema

--- apps/api/services/auth.ts
+++ apps/api/services/auth.ts
@@ -3,7 +3,7 @@ import { prisma } from '@bomber-app/database';
 export const authService = {
   getMockLogin: async () => {
     return await prisma.user.findUnique({
-      where: { id: '2fc0894b-1fb7-4734-a64a-ea24d93883e4' }, // change if need to work with seeded database may be different than mine
+      where: { id: '5ca9200a-ead4-47a9-a10a-b448bf43e302' }, // change if need to work with seeded database may be different than mine
       include: {
         player: {
           include: {
