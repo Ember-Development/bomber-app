7a3a58e4e36d61b405016fb0488e31a2cadd9f0c
braedonwatkins <watkins.braedon@gmail.com>
2025-08-03 17:57:03 -0400

    add types for the join table eventAttendance

---
+++ packages/database/types/eventAttendance.ts
@@ -0,0 +1,24 @@
+import { Prisma, EventAttendance } from '../generated/client';
+
+type Relations = {
+  users: { user: true };
+  events: { event: true };
+};
+
+export type EventAttendanceFE = Omit<
+  Prisma.EventAttendanceGetPayload<{
+    include: { event: true; user: true };
+  }>,
+  'id'
+>;
+
+// this is weird but fixes whatever fucked up since last time
+export type EventAttendanceBE = Omit<EventAttendance, 'id'>;
+
+// FIXME: for some reason this doesnt work... boo
+// export type EventFE = EventDynamic<['attendees']>;
+export type EventAttendanceDynamic<R extends (keyof Relations)[]> =
+  Prisma.EventGetPayload<{
+    include: { [K in R[number]]: true };
+  }>;
+export type EventAttendanceDB = EventAttendanceDynamic<[]>;
