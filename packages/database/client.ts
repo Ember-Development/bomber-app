da3f5cbcfd3e82866f40807d3409f2321e4154cf
braedonwatkins <watkins.braedon@gmail.com>
2025-08-14 11:59:00 -0400

    this was necessary for ssh + autoformat

--- packages/database/client.ts
+++ packages/database/client.ts
@@ -1,6 +1,15 @@
 import { PrismaClient } from './generated/client';
 
 const prisma = new PrismaClient();
+prisma
+  .$connect()
+  .then(() => {
+    console.log('Database connected successfully');
+  })
+  .catch((error) => {
+    console.error('Database connection failed:', error);
+    process.exit(1);
+  });
 
 const globalForPrisma = global as unknown as { prisma: typeof prisma };
 
